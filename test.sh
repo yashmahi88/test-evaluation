81de23dc12da672d66c2396472d576394c42f7d1
yashmahi88 <yash.mahindrakar@einfochips.com>
2024-07-17 09:39:57 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,12 @@
 echo "Enter an hour of the day (in 24-hour format, e.g., 00:00 to 23:59): "
 read hour
 
-# Validate input format (24-hour)
 regex='^([01][0-9]|2[0-3]):([0-5][0-9])$'
 if ! [[ $hour =~ $regex ]]; then
     echo "Invalid input. Please enter an hour in 24-hour format (e.g., 00:00 to 23:59)."
     exit 1
 fi
 
-# Categorize the time of day based on the hour entered
 if [[ "$hour" > "00:00" && "$hour" < "06:01" ]]; then
     echo "Time of day for $hour is: Early Morning"
 elif [[ "$hour" > "06:00" && "$hour" < "12:01" ]]; then
